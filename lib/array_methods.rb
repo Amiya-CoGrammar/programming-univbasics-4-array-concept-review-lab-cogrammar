22e9a3e5f4b4670333ecdb6a7c0df68890158f1b
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-07 11:40:41 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = 0
+  while (i < array.length)
+    index = array.find_index(value_to_find)
+  end
+  p index
 end
+find_element_index([32, 453, 72, 566], 72)
 
 def find_max_value(array)
   # Add your solution here

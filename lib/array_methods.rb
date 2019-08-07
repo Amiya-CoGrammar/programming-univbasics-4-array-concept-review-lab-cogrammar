f8ee4e670940450cc585beb9047755454a201e64
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-07 11:44:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,9 @@ puts index
 find_element_index([32, 453, 72, 566], 72)
 
 def find_max_value(array)
-  # Add your solution here
+  array.max()
 end
+find_max_value([32, 453, 72, 566])
 
 def find_min_value(array)
   # Add your solution here
